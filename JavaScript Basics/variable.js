9626b199b6ac779a95b82abd3e95dd4cd7c6823f
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-10-12 20:08:17 +0530

    JavaScript Basics

---
+++ JavaScript Basics/variable.js
@@ -0,0 +1,7 @@
+var myName = "Kavish";
+var myAge = 22;
+console.log("My name: " + myName);
+console.log("My age: " + myAge); 
+
+alert("Name: " + myName + "Age: " + myAge);
+document.write("Hello " + myName);
