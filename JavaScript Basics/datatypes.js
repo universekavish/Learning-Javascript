dff22c4c1e09bf0cec46a6817bc686e4bbc9660b
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-10-13 13:16:02 +0530

    JavaScript Basics

---
+++ JavaScript Basics/datatypes.js
@@ -0,0 +1,32 @@
+// Number
+var num = 10;
+console.log(num);
+console.log(typeof 10);
+// String
+var myName = "Kavita";
+console.log(typeof "Kavita");
+console.log(myName);
+// Boolean
+var isHere = true;
+console.log(typeof isHere);
+console.log(isHere);
+// Null
+var a = "a";
+console.log(a);
+a = null;
+console.log(typeof a);
+console.log(a);
+// Undefined
+var x;
+console.log(typeof x);
+console.log(x);
+// JavaScript types are dynamic 
+var willChange = "dsksgh";
+console.log(willChange);
+willChange = 5;
+console.log(willChange);
+willChange = true;
+console.log(willChange);
+// use of typeof operator
+console.log(typeof "Kavita");
+console.log(typeof 10);
