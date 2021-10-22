eabcd06933df06947155693a09227f67a432580b
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-10-22 23:54:18 +0530

    Working with arrays

---
+++ Working with arrays/arrayIntro.js
@@ -0,0 +1,27 @@
+var carsILike = ["corvetter", "demon dodge"];
+console.log(carsILike);
+var numbersILike = [5, 2, 6];
+console.log(numbersILike);
+
+// arrays of objects
+var objectsCreated = [
+	{
+		name: "Kavita"
+	}
+]
+console.log(objectsCreated);
+
+// arrays holding functions
+var functionsCreated = [
+	function(){
+
+	},
+	function(){
+
+	}
+]
+
+// array containing mixed items
+// arrays can be nested
+var mixedItems = ["bmw", 2, ["corvette"]];
+console.log(mixedItems);
