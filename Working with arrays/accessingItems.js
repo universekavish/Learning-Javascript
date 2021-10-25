77831d98da60307afb35a10c47167fa899b6aa36
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-10-25 22:22:58 +0530

    Working with arrays

---
+++ Working with arrays/accessingItems.js
@@ -0,0 +1,7 @@
+var carsILike = {"bmw", "kia", "mercedes", "fiat"};
+
+carsILike[0] = "corvette";
+
+carsILike[carsILike.length] = "polo";
+
+console.log(carsILike["0"]);
---
+++ Working with arrays/addingItems.js
@@ -0,0 +1,13 @@
+var carsILike = {"bmw", "kia", "mercedes", "fiat"};
+
+// add element at the last index
+carsILike.push("corvette");
+carsILike.push("limo");
+carsILike.push("tesla");
+
+// add element to the 0th index
+carsILike.unshift("Tavera");
+carsILike.unshift("Fumero");
+carsILike.unshift("Kimero");
+
+console.log(carsILike);
