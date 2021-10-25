7709f8717cbf36c21dfac8655727cea5f36c9778
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-10-25 22:27:26 +0530

    Working with arrays

---
+++ Working with arrays/TwoDArrays.js
@@ -0,0 +1,7 @@
+var whatILike = [["polo", "golf"], ["potato"], ["javascript"]];
+
+for(var i = 0; i < whatILike.length; i++){
+	for (var j = 0; j < whatILike[i].length; j++) {
+		console.log(whatILike[i][j]);
+	}
+}
---
+++ Working with arrays/removingItems.js
@@ -0,0 +1,8 @@
+var carsILike = {"bmw", "kia", "mercedes", "fiat"};
+
+// removing last item from array
+carsILike.pop();
+
+// removin first item from array
+carsILike.shift();
+
